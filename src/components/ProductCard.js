6ac4dbf75c015ac8c7c1549123e2485e0dad4ba3
mir4cles <jurimiralles@gmail.com>
2020-05-19 17:48:17 +0200

    link in productcard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./ProductCard.css";
+import { Link } from "react-router-dom";
 
 export default function ProductCard(props) {
 	return (
 		<div key={props.id} className="ProductCard">
 			<div className="product">
 				<div className="title">
-					<h3>{props.name}</h3>
-					<img src={props.image} height="300px" />
+					<Link className="" to={`/product/${props.id}`}>
+						<h3>{props.name}</h3>
+						<img src={props.image} height="300px" />
+					</Link>
 				</div>
 				<div className="meta">
 					{props.tags.map((tag, i) => {
