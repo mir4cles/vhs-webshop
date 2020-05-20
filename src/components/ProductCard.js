babc49063055271745ceb3a4dd7f35df9ca95c0a
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-05-20 17:41:06 +0200

    added addToCartButtons components to all pages

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./ProductCard.css";
 import { Link } from "react-router-dom";
+import AddToCart from "./AddToCartButtons";
 
 export default function ProductCard(props) {
-	return (
-		<div key={props.id} className="ProductCard">
-			<div className="product">
-				<div className="title">
-					<Link className="" to={`/product/${props.id}`}>
-						<h3>{props.name}</h3>
-						<img src={props.image} height="300px" />
-					</Link>
-				</div>
-				<div className="meta">
-					{props.tags.map((tag, i) => {
-						return (
-							<span key={i} className="tag">
-								{tag}
-							</span>
-						);
-					})}
-				</div>
-				<p>
-					€{props.price} <button>Add to cart</button>
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div key={props.id} className="ProductCard">
+      <div className="product">
+        <div className="title">
+          <Link className="" to={`/product/${props.id}`}>
+            <h3>{props.name}</h3>
+            <img src={props.image} height="300px" />
+          </Link>
+        </div>
+        <div className="meta">
+          {props.tags.map((tag, i) => {
+            return (
+              <span key={i} className="tag">
+                {tag}
+              </span>
+            );
+          })}
+        </div>
+        <p>€{props.price}</p>
+        <AddToCart id={props.id} />
+      </div>
+    </div>
+  );
 }
